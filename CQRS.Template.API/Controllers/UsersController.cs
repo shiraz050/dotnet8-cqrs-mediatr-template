606933fc01582ec3f80a3c952195ca46c6dd36b9
shiraz050 <shirazahmed847@gmail.com>
2025-06-01 13:55:17 +0500

    feat: Add UsersController for user creation endpoint
    - Introduced `UsersController` in `CQRS.Template.API.Controllers`.
    - Added `[ApiController]` and `[Route("api/[controller]")]` attributes.
    - Implemented `CreateUser` method to handle HTTP POST requests.
    - Accepts `CreateUserCommand` and returns user ID in response.

---
+++ CQRS.Template.API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using CQRS.Template.Application.Features.Users.Commands.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.Template.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand command)
+    {
+        var id = await _mediator.Send(command);
+        return Ok(new { UserId = id });
+    }
+}
